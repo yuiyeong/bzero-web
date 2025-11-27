ef7a8793f1969bdf5770008862d2f3bb948385de
yuiyeong <joyuiyeong@gmail.com>
2025-11-27 17:30:27 +0900

    feat: 기본적인 페이지들 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-client.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { router } from "@/root-route.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/root-route.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, Navigate } from "react-router";
+import OnboardingPage from "@/pages/onboarding-page.tsx";
+import SignInPage from "@/pages/sign-in-page.tsx";
+import SignUpPage from "@/pages/sign-up-page.tsx";
+import ProfilePage from "@/pages/profile-page.tsx";
+import IndexPage from "@/pages/index-page.tsx";
+import MainLayout from "@/components/layout/main-layout.tsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/onboarding",
+    element: <OnboardingPage />,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignInPage />,
+        handle: { title: "로그인" },
+      },
+      {
+        path: "/sign-up",
+        element: <SignUpPage />,
+        handle: { title: "회원가입" },
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+        handle: { title: "프로필" },
+      },
+      {
+        path: "/",
+        element: <IndexPage />,
+        handle: { title: "홈" },
+      },
+      {
+        path: "*",
+        element: <Navigate to="/" />,
+      },
+    ],
+  },
+]);
